0534c0aec74a9b081b3df286d3fe99e5d212887e
Oleg Andrus <ol.rus12@ya.ru>
2026-02-18 20:47:19 +0300

    Семинар 4. Тестирование

---
+++ src/test/java/org/example/repository/ConstellationRepositoryMockTest.java
@@ -0,0 +1,47 @@
+package org.example.repository;
+
+import org.example.SatelliteConstellation;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Тест выполнения репозитория")
+class ConstellationRepositoryMockTest {
+    private static final String CONSTELLATION_1 = "testConstellation1";
+    private static final String CONSTELLATION_2 = "testConstellation2";
+
+    @Mock
+    private ConstellationRepository repository;
+
+    @Test
+    void testRepository() {
+        SatelliteConstellation constellation1 = new SatelliteConstellation(CONSTELLATION_1);
+        SatelliteConstellation constellation2 = new SatelliteConstellation(CONSTELLATION_2);
+
+        Map<String, SatelliteConstellation> constellations = Map.of(
+                CONSTELLATION_1, constellation1,
+                CONSTELLATION_2, constellation2
+        );
+
+        when(repository.containsConstellation(CONSTELLATION_1)).thenReturn(true);
+        when(repository.containsConstellation(CONSTELLATION_2)).thenReturn(true);
+
+        when(repository.getConstellations()).thenReturn(constellations);
+
+        assertTrue(repository.containsConstellation(CONSTELLATION_1));
+        assertTrue(repository.containsConstellation(CONSTELLATION_2));
+
+        assertEquals(2, repository.getConstellations().size());
+    }
+}
